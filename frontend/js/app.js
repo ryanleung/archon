e985ce2fa025fee4270bf658e5351d6607046b73
Michael Jun <stumichael9109@berkeley.edu>
2014-06-24 18:11:08 -0700

    added var playlist to start dynamic html

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,1 +1,19 @@
 App = Ember.Application.create();
+
+App.Router.map(function() {
+	this.resource('about');
+})
+
+var playlists = [{
+  id: '1',
+  title: "kpop",
+  author: { name: "d2h" },
+  date: new Date('12-27-2012'),
+  listener_count: 18
+  }, {
+  id: '2',
+  title: "The Parley Letter",
+  author: { name: "d2h" },
+  date: new Date('12-24-2012'),
+  listener_count: 20
+  }]
