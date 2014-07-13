69daa32db5f767bc98501a9cb5881ada8eb4d969
ryanluv <ryan@luvocracy.com>
2014-07-12 19:01:47 -0700

    making UI for sidebar

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -23,23 +23,38 @@ App.ApplicationRoute = Ember.Route.extend({
       this.get('store').push('playlist', {id: id_count, title: playlist_name})
       id_count += 1;
     },
+    addVideo: function() {
+      alert("hi")
+    },
     youtubeSearch: function(search_query) {
-      api_key = "AIzaSyAhe1BqglbuLH3s2ZUBacjEoxTQ7ZKD0-k"
-      result = $.get("https://www.googleapis.com/youtube/v3/search", 
-                      { key: api_key,
-                        part: "snippet",
-                        q: search_query
-                      },
-                      function(data) {
-                        searchResults = data.items;
-                        for (i = 0; i < searchResults.length; i++) {
-                          $('#myResults').append(searchResults[i].snippet.title);
-                        }
-                      }
-                    ); 
+      searchResults = [{etag: "FOuwADrXJjsTKgUIQJoQC6nKNFY/7BoRtJF13QuMNFxwC3Ec8xUPaCc", description: "TAEYANG - 눈,코,입 (EYES, NOSE, LIPS) M/V] #TAEYANG #RISE #EYESNOSELIPS * 눈, 코, 입(EYES, NOSE, LIPS) COVER PROJECT BY YOU Submission ...", url: "https://i.ytimg.com/vi/UwuAPyOImoI/default.jpg", title: "TAEYANG - 눈,코,입 (EYES, NOSE, LIPS) M/V", videoId: "UwuAPyOImoI"},
+                       {etag: "FOuwADrXJjsTKgUIQJoQC6nKNFY/7BoRtJF13QuMNFxwC3Ec8xUPaCc", description: "TAEYANG - 눈,코,입 (EYES, NOSE, LIPS) M/V] #TAEYANG #RISE #EYESNOSELIPS * 눈, 코, 입(EYES, NOSE, LIPS) COVER PROJECT BY YOU Submission ...", url: "https://i.ytimg.com/vi/UwuAPyOImoI/default.jpg", title: "TAEYANG - 눈,코,입 (EYES, NOSE, LIPS) M/V", videoId: "UwuAPyOImoI"}];
+      $('#myResults').append("<ul>")
+      for (i = 0; i < searchResults.length; i++) {
+        $('#myResults').append("<li>");
+        title = searchResults[i].title;
+        image = searchResults[i].url;
+        $('#myResults').append("<img src=" + image + ">" + title);
+        $('#myResults').append('<button {{action "addVideo"}}>+</button>')
+        $('#myResults').append("</li>");
+      }
+      $('#myResults').append("</ul>");
+    //   api_key = "AIzaSyAhe1BqglbuLH3s2ZUBacjEoxTQ7ZKD0-k"
+    //   result = $.get("https://www.googleapis.com/youtube/v3/search", 
+    //                   { key: api_key,
+    //                     part: "snippet",
+    //                     q: search_query
+    //                   },
+    //                   function(data) {
+    //                     searchResults = data.items;
+    //                     for (i = 0; i < searchResults.length; i++) {
+    //                       $('#myResults').append(searchResults[i].snippet.title);
+    //                     }
+    //                   }
+    //                 ); 
 
-    }
-  }
+    // }
+  }}
 });
 
 App.Playlist = DS.Model.extend({
