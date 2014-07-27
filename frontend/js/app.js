bd142f74335c1aac3ee094ca6fd35aaba1fabb05
Michael Jun <stumichael9109@berkeley.edu>
2014-07-26 23:41:39 -0700

    working on rendering search results

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -25,20 +25,32 @@ App.ApplicationRoute = Ember.Route.extend({
     },
     addVideo: function() {
       alert("hi")
-    },
+    }
+  }
+});
+
+App.SearchRoute = Ember.Route.extend({
+  model: function () {
+    return this.store.find('video');
+  }
+});
+
+App.SearchController = Ember.ArrayController.extend({
+  actions: {
     youtubeSearch: function(search_query) {
       searchResults = [{etag: "FOuwADrXJjsTKgUIQJoQC6nKNFY/7BoRtJF13QuMNFxwC3Ec8xUPaCc", description: "TAEYANG - 눈,코,입 (EYES, NOSE, LIPS) M/V] #TAEYANG #RISE #EYESNOSELIPS * 눈, 코, 입(EYES, NOSE, LIPS) COVER PROJECT BY YOU Submission ...", url: "https://i.ytimg.com/vi/UwuAPyOImoI/default.jpg", title: "TAEYANG - 눈,코,입 (EYES, NOSE, LIPS) M/V", videoId: "UwuAPyOImoI"},
                        {etag: "FOuwADrXJjsTKgUIQJoQC6nKNFY/7BoRtJF13QuMNFxwC3Ec8xUPaCc", description: "TAEYANG - 눈,코,입 (EYES, NOSE, LIPS) M/V] #TAEYANG #RISE #EYESNOSELIPS * 눈, 코, 입(EYES, NOSE, LIPS) COVER PROJECT BY YOU Submission ...", url: "https://i.ytimg.com/vi/UwuAPyOImoI/default.jpg", title: "TAEYANG - 눈,코,입 (EYES, NOSE, LIPS) M/V", videoId: "UwuAPyOImoI"}];
-      $('#myResults').append("<ul>")
+      //$('#myResults').append("<ul>")
       for (i = 0; i < searchResults.length; i++) {
-        $('#myResults').append("<li>");
+        //$('#myResults').append("<li>");
         title = searchResults[i].title;
         image = searchResults[i].url;
-        $('#myResults').append("<img src=" + image + ">" + title);
-        $('#myResults').append('<button {{action "addVideo"}}>+</button>')
-        $('#myResults').append("</li>");
+        this.get('store').createRecord('video', {title: title})
+        //$('#myResults').append("<img src=" + image + ">" + title);
+        //$('#myResults').append('<button {{action "addVideo"}}>+</button>')
+        //$('#myResults').append("</li>");
       }
-      $('#myResults').append("</ul>");
+      //$('#myResults').append("</ul>");
     //   api_key = "AIzaSyAhe1BqglbuLH3s2ZUBacjEoxTQ7ZKD0-k"
     //   result = $.get("https://www.googleapis.com/youtube/v3/search", 
     //                   { key: api_key,
@@ -54,7 +66,8 @@ App.ApplicationRoute = Ember.Route.extend({
     //                 ); 
 
     // }
-  }}
+    }
+  }
 });
 
 App.Playlist = DS.Model.extend({
