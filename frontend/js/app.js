55afa2427a91c1d65ad55125117a1019068a11f1
Ryan Leung <ryanleung@berkeley.edu>
2014-08-03 16:14:55 -0700

    added skeleton for organization

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,15 +1,140 @@
+// app.js
+// This file is responsible for the main logic of the Ember Application.
+// This is designed to be broken up into different files once we get the
+// hang of things. (ex. all of the controllers will be in controllers.js,
+// models in models.js, etc.)
+// Right now, this file is in chronological order with how the application runs.
+
+
+/**
+---- APP INITIALIZATION ----
+This section deals with instantiating the Ember application and
+sets up custom options, like whether or not to use a fixture
+for the DB.
+**/
 App = Ember.Application.create();
 App.ApplicationAdapter = DS.FixtureAdapter.extend();
-id_count = 3;
+
+/**
+---- APP ROUTER ----
+This section deals with setting up the router. The router is 
+responsible for setting up routes. Routes are responsible
+for linking the current url to its respective controller.
+(ex. www.app.com/playlist => PlaylistRoute => PlaylistController)
+**/
 App.Router.map(function() {
-  this.resource('about');
-  this.resource('home');
-  this.resource('playlist', { path: 'playlist/:playlist_id'},
+  this.route('about');
+  this.route('home');
+  this.resource('playlists', { path: 'playlists/:playlist_id'},
     function() {
-      this.resource('video', {path: 'video/:video_id'})
+      this.resource('videos', {path: 'videos/:video_id'})
     }
   );
 });
+
+/**
+---- APP ROUTES ----
+This section deals with defining routes. Routes are responsible
+for initial setup of the controllers and rendering the appropriate
+template to the screen using {{outlet}}
+
+ex., initial setup
+App.IndexRoute = Ember.Route.extend({
+  setupController: function(controller) {
+    // Set the IndexController's `title`
+    controller.set('title', "My App");
+  }
+});
+
+ex., rendering template
+App.PostsRoute = Ember.Route.extend({
+  renderTemplate: function() {
+    this.render('favoritePost');
+  }
+});
+**/
+
+App.ApplicationRoute = Ember.Route.extend({ // This route gets called every time the app loads
+  
+});
+
+App.IndexRoute = Ember.Route.extend({ // This route gets called when '/' path is loaded
+
+});
+
+App.PlaylistsRoute = Ember.Route.extend({ // This route gets called when '/playlists' path is loaded
+
+});
+
+App.VideosRoute = Ember.Route.extend({ // This route gets called when '/playlists/:playlist_id/videos' path is loaded
+
+});
+
+/**
+---- APP CONTROLLERS ----
+This section deals with defining controllers. Controllers
+are responsible for giving stuff to the templates to display,
+and their logics are used after being routed to by their respective
+route (ex. App.PlaylistRoute calls App.PlaylistController)
+
+ex. 
+App.SongController = Ember.ObjectController.extend({
+  soundVolume: 1
+});
+**/
+
+App.ApplicationController = Ember.ArrayController.extend({
+
+});
+
+App.IndexController = Ember.ObjectController.extend({
+
+});
+
+App.PlaylistsController = Ember.ArrayController.extend({
+
+});
+
+App.VideosController = Ember.ArrayController.extend({
+
+});
+
+/**
+---- APP MODELS ----
+This secion deals with defining models. Models are responsible
+for defining objects that will be persisted and used by the
+controllers
+
+ex. 
+App.Person = DS.Model.extend({
+  firstName: attr(),
+  lastName: attr(),
+  birthday: attr()
+});
+**/
+
+App.Playlist = DS.Model.extend({
+  title: DS.attr('string'),
+  watcher_count: DS.attr('number'),
+  videos: DS.hasMany('video', {async: true}),
+});
+
+App.Video = DS.Model.extend({
+  title: DS.attr('string'),
+  playlist: DS.belongsTo('playlist'),
+  url: DS.attr('string')
+});
+
+/**
+---- APP VIEWS ----
+This section deals with defining views.
+
+ex.
+var view = Ember.View.create({
+  templateName: 'say-hello',
+  name: "Bob"
+});
+**/
 
 App.ApplicationRoute = Ember.Route.extend({
   searchResults: [1,2,3],
@@ -21,7 +146,6 @@ App.ApplicationRoute = Ember.Route.extend({
   actions: {
     addPlaylist: function(playlist_name) {
       this.get('store').push('playlist', {id: id_count, title: playlist_name})
-      id_count += 1;
     },
     addVideo: function() {
       alert("hi")
@@ -59,17 +183,10 @@ App.ApplicationRoute = Ember.Route.extend({
   }
 });
 
-App.Playlist = DS.Model.extend({
-  title: DS.attr('string'),
-  watcher_count: DS.attr('number'),
-  videos: DS.hasMany('video', {async: true}),
-});
-
-App.Video = DS.Model.extend({
-  title: DS.attr('string'),
-  playlist: DS.belongsTo('playlist'),
-  url: DS.attr('string')
-});
+/**
+---- APP FIXTURES ----
+Temporary DB objects
+**/
 
 App.Playlist.FIXTURES = [
   {
