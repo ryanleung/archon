5131e007dc1f413cffe1c160200af4f720ea3912
Ryan Leung - Vendor <rleung@l-sb8r1l9fd5-m.homeoffice.wal-mart.com>
2014-08-06 17:26:50 -0700

    trying to get youtube player working, not done yet

--- frontend/js/yt_player.js
+++ frontend/js/yt_player.js
@@ -1,2 +1,21 @@
-var API_KEY = "AIzaSyAhe1BqglbuLH3s2ZUBacjEoxTQ7ZKD0-k"
+var YoutubePlayer = function() {};
 
+YoutubePlayer.prototype.init = function() {
+    var youtube_params = { allowScriptAccess: "always" };
+    var youtube_atts = { id: "youtube-player" };
+    swfobject.embedSWF("http://www.youtube.com/apiplayer?enablejsapi=1&version=3",
+      "youtube-player", "854", "480", "8", null, null, youtube_params, youtube_atts);
+}
+
+/* Youtube Player Callbacks */
+function onYouTubePlayerReady(playerId) {
+    // alert('hi');
+    // debugger;
+    player.youtube_player = $('#youtube-player').get(0);
+    player.youtube_player.addEventListener('onStateChange', 'onYouTubeStateChange');
+}
+
+$(function() {
+    yt_player = new YoutubePlayer();
+    yt_player.init();
+});
